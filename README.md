469ffb6d92f70354746757040c1632a3854be174
ChandraKent <54602807+ChandraKent@users.noreply.github.com>
2019-12-14 22:40:50 -0500

    ETL Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ETL_Project
 Extract, Transform, Load
+
+This is our ETL Project. We have gathered Crime data from the DMV area (D.C., Bethesda, Arlington).
---
+++ config.py
@@ -0,0 +1,8 @@
+sf_grant_type = 'password'
+sf_client_id = '3MVG9LBJLApeX_PD7MppZu1xQ7O8LjYl1gZMW08MMfYavaQG20uQ4smvccW7J2h.VamD1O3ipoQ.DM6uwBOY4'
+sf_client_secret = '632B1A8A6D379B2469794A910379BDF746D642172DE35D1A5937C3915114E517'
+sf_username = 'chandra@mimosa.com'
+sf_password = 'Djangotron1' # needs to be passowrd and security token combined
+sf_security_token = 'cTRFCTF5SIC2qFXAVZnkYP7jx'
+sf_endpoint = 'https://login.salesforce.com/services/oauth2/token'
+
